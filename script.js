9d7482cf2101489bc3a20194c1d715567ed72389
livia16livia <livia.dalsico.nogueira@escola.pr.gov.br>
2026-06-30 10:17:35 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", function () {
+    alert("Bem-vindo à Essência Brasil! Conheça nossas fragrâncias exclusivas.");
+});
